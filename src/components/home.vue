79eb801d5cf710e168b1130531ba7d4fa4765b2f
lifan zhu <a1253308969@gmial.com>
2020-05-16 11:17:08 +0800

    主頁佈局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="aa">
-      <el-button type="info" @click="logout">登出</el-button>
-    </div>
+<el-container>
+  <el-header>   <el-button type="info" @click="logout">登出</el-button></el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 <script>
 export default {
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/zh-TW'
 import locale from 'element-ui/lib/locale'
 
@@ -9,4 +9,8 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
